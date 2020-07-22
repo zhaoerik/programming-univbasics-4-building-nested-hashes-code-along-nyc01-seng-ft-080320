431f5d37f142905f7eb4b2d2bc3119d314db5d6c
Erik Zhao <erikzhao98@gmail.com>
2020-07-22 22:55:29 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -33,7 +33,7 @@ def third_challenge
       :heroine => {
         #your key/value pairs here
         :name => "Juliet",
-        :age => 15,
+        :age => "15",
         :status => "alive"
         },
       :heroine_friends => []
