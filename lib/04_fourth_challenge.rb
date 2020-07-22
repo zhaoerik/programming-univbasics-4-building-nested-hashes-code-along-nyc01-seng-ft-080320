50be722d655ccd1249e208de65c42cd1cb8e087a
Erik Zhao <erikzhao98@gmail.com>
2020-07-22 22:54:38 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -13,7 +13,7 @@ def fourth_challenge
         :age => "18",
         :attitude => "hot-headed"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
@@ -27,6 +27,6 @@ def fourth_challenge
    }
 }
 
-  
+
 
 end
